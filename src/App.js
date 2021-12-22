956eeca62b4391da784fe0890766a698ce8df3b8
arie-azh-ari <Arie.Azhari@medirecords.com>
2021-12-22 07:51:24 +0700

    Timer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import Time from "./time.svg"
+import TimeWhite from "./timeWhite.svg"
 import Mes from "./message.svg"
+import MesWhite from "./messageWhite.svg"
 import Ari from "./arri.jpg"
 
 const App = () => {
+  const [dateState, setDateState] = useState(new Date());
+  const [isMorning, setIsMorning] = useState(true);
+  useEffect(() => {
+    setInterval(() =>
+      setDateState(new Date()), 1000);
+    return () => {
+      clearInterval()
+    }
+  }, []);
+
+  useEffect(() => {
+    const hour = dateState.getHours()
+    if (hour > 17 || hour < 5) {
+      setIsMorning(false)
+    }else {
+      setIsMorning(true)
+    }
+
+  }, [dateState])
   return (
-    <div className="w-full max-h-screen h-screen bg-white flex justify-center items-center">
-      <Name />
-      <Messages />
-      <DateTime />
+    <div className={`w-full max-h-screen h-screen transition-colors duration-1000 ${isMorning ? 'bg-white' : 'bg-black'} flex justify-center items-center`}>
+      <Name isMorning={isMorning}/>
+      <Messages isMorning={isMorning}/>
+      <DateTime isMorning={isMorning}/>
     </div>
   )
 }
 
-const Name = () => {
+const Name = ({isMorning}) => {
   return (
-    <div className="font-serif text-xl tracking-wider md:text-3xl xl:text-5xl italic">Lily Putri Marito</div>
+    <div className={`font-serif text-xl tracking-wider md:text-3xl xl:text-5xl italic transition-colors duration-1000 ${isMorning ? 'text-black':'text-white'}`}>Lily Putri Marito</div>
   )
 }
 
-const Modal = ({ setIsOpen }) => {
+const Modal = ({ setIsOpen, isMorning }) => {
   const message = process.env.REACT_APP_MESSAGES
   return (
     <div className="absolute w-full h-screen z-10 bg-black bg-opacity-40 flex justify-center items-center">
       {/* modal */}
-      <div className="relative bg-white rounded-lg w-2/3 md:w-1/4 p-4 flex flex-col space-y-3">
-        <div className="text-sm italic text-left">Today's Greeting</div>
+      <div className={`relative ${isMorning ? 'bg-white' : 'bg-gray-900'} rounded-lg w-2/3 md:w-1/4 p-4 flex flex-col space-y-3`}>
+        <div className={`text-sm italic text-left ${isMorning ? 'text-black' : 'text-white'}`}>Today's Greeting</div>
         <div className="flex space-x-2 items-start justify-end">
-          <div className="text-sm w-auto bg-gray-400 p-2 rounded-xl text-white">{message}</div>
+          <div className={`text-sm w-auto p-2 rounded-xl ${isMorning ? 'bg-gray-400 text-white' :'text-black bg-gray-400'}`}>{message}</div>
           <div>
             <div className="h-10 w-10 rounded-full bg-gray-400">
               <img src={Ari} className="w-full h-full rounded-full object-cover" alt="logo" />
@@ -35,20 +56,20 @@ const Modal = ({ setIsOpen }) => {
           </div>
         </div>
         <div className="pt-4 flex justify-end">
-          <button onClick={() => setIsOpen(false)} className="px-8 border py-1 text-sm bg-blue-400 text-white rounded-md">Close</button>
+          <button onClick={() => setIsOpen(false)} className={`px-8 border py-1 text-sm ${isMorning ? 'bg-blue-400 text-white' : 'text-black bg-gray-400'} rounded-md`}>Close</button>
         </div>
       </div>
     </div>
   )
 }
 
-const Messages = () => {
+const Messages = ({isMorning}) => {
   const message = process.env.REACT_APP_MESSAGES
   const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       {isOpen && (
-        <Modal setIsOpen={setIsOpen} />
+        <Modal setIsOpen={setIsOpen} isMorning={isMorning} />
       )}
       <div className="absolute bottom-0 right-0 p-4 md:p-8 flex space-x-2 items-center">
         <div onClick={() => setIsOpen(!isOpen)} className="relative cursor-pointer">
@@ -56,22 +77,22 @@ const Messages = () => {
             message &&
             <div className="absolute -top-2 -right-2 bg-red-600 rounded-full h-4 w-4 md:h-6 md:w-6 text-xs text-white justify-center items-center flex">1</div>
           }
-          <img src={Mes} className="h-8 md:h-12" alt="logo" />
+          <img src={isMorning ? Mes : MesWhite} className="h-8 md:h-12" alt="logo" />
         </div>
       </div>
     </>
   )
 }
 
-const DateTime = () => {
+const DateTime = ({isMorning}) => {
   const [dateState, setDateState] = useState(new Date());
   useEffect(() => {
     setInterval(() =>
       setDateState(new Date()), 1000);
   }, []);
   return (
-    <div className="absolute bottom-0 left-0 p-4 flex space-x-2 items-center">
-      <img src={Time} className="h-3 md:h-5" alt="logo" />
+    <div className={`absolute bottom-0 left-0 p-4 flex space-x-2 items-center ${isMorning ? 'text-black':'text-white'}`}>
+      <img src={isMorning ? Time : TimeWhite} className="h-3 md:h-5 text-white" alt="logo" />
 
       <div className="text-xs md:text-base">
         {dateState.toLocaleString('en-US', {
